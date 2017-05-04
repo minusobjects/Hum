2d58fb3333f3744936d631fa4402a2891e0154f4
Michael Newton <mn@one-timepad.com>
2017-05-04 12:46:10 -0400

    Re-add object

--- test_canvas.js
+++ test_canvas.js
@@ -18,56 +18,56 @@ const ctx = canvasEl.getContext('2d');
 // }
 
 // load default audio - seems to work fine with multiple files
-// BUT having errors locally!
-let defaultHowl1 = new Howl({
-  preload: true,
-  volume: 0.5,
-  src: 'defaults/neverturnback.mp3',
-  onload: function() {
-    console.log('Loaded Default 1!');
-  },
-  onplay: function() {
-    console.log('Playing Default 1!');
-  },
-  onend: function() {
-    console.log('Finished Default 1!');
-  }
-});
-
-let defaultHowl2 = new Howl({
-  preload: true,
-  volume: 0.5,
-  src: 'defaults/80s_vibe.mp3',
-  onload: function() {
-    console.log('Loaded Default 2!');
-  },
-  onplay: function() {
-    console.log('Playing Default 2!');
-  },
-  onend: function() {
-    console.log('Finished Default 2!');
-  }
-});
-
-let defaultHowl3 = new Howl({
-  preload: true,
-  volume: 0.5,
-  src: 'defaults/rave_digger.mp3',
-  onload: function() {
-    console.log('Loaded Default 3!');
-  },
-  onplay: function() {
-    console.log('Playing Default 3!');
-  },
-  onend: function() {
-    console.log('Finished Default 3!');
-  }
-});
-
+// BUT having errors locally! So, gonna turn it off for now.
+// let defaultHowl1 = new Howl({
+//   preload: true,
+//   volume: 0.5,
+//   src: 'defaults/neverturnback.mp3',
+//   onload: function() {
+//     console.log('Loaded Default 1!');
+//   },
+//   onplay: function() {
+//     console.log('Playing Default 1!');
+//   },
+//   onend: function() {
+//     console.log('Finished Default 1!');
+//   }
+// });
+//
+// let defaultHowl2 = new Howl({
+//   preload: true,
+//   volume: 0.5,
+//   src: 'defaults/80s_vibe.mp3',
+//   onload: function() {
+//     console.log('Loaded Default 2!');
+//   },
+//   onplay: function() {
+//     console.log('Playing Default 2!');
+//   },
+//   onend: function() {
+//     console.log('Finished Default 2!');
+//   }
+// });
+//
+// let defaultHowl3 = new Howl({
+//   preload: true,
+//   volume: 0.5,
+//   src: 'defaults/rave_digger.mp3',
+//   onload: function() {
+//     console.log('Loaded Default 3!');
+//   },
+//   onplay: function() {
+//     console.log('Playing Default 3!');
+//   },
+//   onend: function() {
+//     console.log('Finished Default 3!');
+//   }
+// });
+//
 let soundObj = {};
-soundObj['audio1'] = defaultHowl1;
-soundObj['audio2'] = defaultHowl2;
-soundObj['audio3'] = defaultHowl3;
+// soundObj['audio1'] = defaultHowl1;
+// soundObj['audio2'] = defaultHowl2;
+// soundObj['audio3'] = defaultHowl3;
 
 const colorInfoButton = document.getElementById('colorInfoButton');
 colorInfoButton.addEventListener('click', getColorInfo, false);
@@ -75,7 +75,7 @@ colorInfoButton.addEventListener('click', getColorInfo, false);
 function getColorInfo(){
   let pixelInfo;
   console.log('You clicked color button!');
-  pixelInfo = ctx.getImageData(0,0,20,canvasEl.height);
+  pixelInfo = ctx.getImageData(0,0,2,canvasEl.height);
   debugger
 }
 
